2861cfce52a6a5bd0aa4ff058561f2c05625957c
mateuszfranke <mateuszfranke@icloud.com>
2020-07-27 06:58:50 +0200

    manage state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,34 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+    state = {
+        persons:[
+            {name:"Max", age:"28"},
+            {name:"Mateusz", age:"29"},
+            {name:"Natalia", age:"28"},
+        ],
+        otherState: "some other value"
+    }
+
+    switchNameHandler = () => {
+      //DON'T change state like this =>  this.state.persons[0].name = "Maximilian";
+        this.setState({
+            persons:[
+                {name:"Maximilian", age:"28"},
+                {name:"Mateusz", age:"29"},
+                {name:"Natalia", age:"18"},
+            ]
+        })
+    }
+
   render() {
     return (
       <div className="App">
           <h1>Hi, I'm a React App</h1>
-        <Person/>
+          <button onClick={this.switchNameHandler}>Switch name</button>
+          <Person name = {this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name = {this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: racing</Person>
+          <Person name = {this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
 
