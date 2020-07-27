b857eb19e9be412c9996068994293ad12c22479e
mateuszfranke <mateuszfranke@icloud.com>
2020-07-27 06:11:57 +0200

    add 'dumb' person componenent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <h1>Hi, I'm a React App</h1>
+        <Person/>
       </div>
     );
+
+/*    return React.createElement(
+        'div',null,
+            React.createElement('h1',{className:"App"},"I'm a React App!")
+    );*/
   }
 }
 
